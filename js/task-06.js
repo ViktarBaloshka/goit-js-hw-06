f9c50863fa150a7e7b34a394e33f1e7c7e4855ef
ViktarBaloshka <vbolosko@gmail.com>
2022-04-20 13:34:29 +0200

    task-08

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,11 @@
-const textInput = document.querySelector("#validation-input");
+const textInput = document.querySelector('#validation-input');
 
-textInput.addEventListener("blur", (event) => {
-  if (event.currentTarget.dataset.length <= event.currentTarget.value.length) {
-    textInput.classList.add("valid");
-    textInput.classList.remove("invalid");
+textInput.addEventListener('blur', event => {
+  if (event.currentTarget.value.length == event.currentTarget.dataset.length) {
+    textInput.classList.add('valid');
+    textInput.classList.remove('invalid');
   } else {
-    textInput.classList.add("invalid");
-    textInput.classList.remove("valid");
+    textInput.classList.add('invalid');
+    textInput.classList.remove('valid');
   }
 });
