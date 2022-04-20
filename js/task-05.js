985b1cba4ca4c5419438fc90d157fb8a1de81997
ViktarBaloshka <vbolosko@gmail.com>
2022-04-20 13:03:33 +0200

    task-06_2

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,8 @@
-const inputName = document.querySelector("#name-input");
-const outputName = document.querySelector("#name-output");
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
 
-inputName.addEventListener("input", (event) => {
-  outputName.textContent = event.currentTarget.value
-    ? event.currentTarget.value
-    : "Anonymous";
+inputName.addEventListener('input', event => {
+  if (event.currentTarget.value) {
+    outputName.textContent = event.currentTarget.value;
+  } else outputName.textContent = 'Anonymous';
 });
